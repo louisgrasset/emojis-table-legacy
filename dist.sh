1f01750fe7d01bc4f46660680732009d9da81997
Louis Grasset <louisgrasset@protonmail.com>
2018-09-30 09:45:57 +0200

    :newspaper: Add dist script

---
+++ dist.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+rm -R build 2>/dev/null
+mkdir "build"
+cp -R ./src/*.* ./src/icons/ ./build/
+rm ./build/icons/*.psd
+cd ./build/
+zip -r ../build/build.zip ./
+cd ../
+rm -R dist 2>/dev/null
+mkdir "dist"
+cp ./build/build.zip ./dist/emojis-table-chrome-X.XX.zip
+mv ./build/build.zip ./dist/emojis-table-firefox-X.XX.xpi
